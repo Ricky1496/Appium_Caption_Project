e30b78d1df572805a6507b27b1c590f2fc54cb3a
Ricky1496 <rickyabraham43@gmail.com>
2023-04-04 18:41:12 +0530

    Minor changes

---
+++ test_data/config.py
@@ -0,0 +1,7 @@
+dic = {
+  "deviceName": "Pixel 4 API 33",
+  "platformName": "Android",
+  "platformVersion": "13",
+  "udid": "emulator-5556",
+  "app": "C:/Users/150234/AppData/Local/Android/Sdk/platform-tools/naukri-com-17-1.apk"
+}
